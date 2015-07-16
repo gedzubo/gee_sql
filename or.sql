1bdfaa4386dbc8354ea7f5d12647de86656479bf
Gediminas <gediminas.zubovicius@gmail.com>
2015-07-16 20:56:55 +0100

    or

---
+++ or.sql
@@ -0,0 +1,21 @@
+USE test;
+DROP TABLE IF EXISTS predecessors;
+
+CREATE TABLE predecessors
+(
+id int,
+predecessor_id int
+);
+
+INSERT INTO predecessors VALUES (1, 2);
+INSERT INTO predecessors VALUES (2, 6);
+INSERT INTO predecessors VALUES (3, NULL);
+INSERT INTO predecessors VALUES (4, NULL);
+INSERT INTO predecessors VALUES (5, NULL);
+INSERT INTO predecessors VALUES (6, 8);
+INSERT INTO predecessors VALUES (7, NULL);
+INSERT INTO predecessors VALUES (8, NULL);
+
+SELECT *
+FROM predecessors
+WHERE predecessors.id > 5 OR predecessors.predecessor_id IS NULL;
